25ef388fd15a01c44e0bb47694042a764f0a1440
PrzemekBiernat1998 <przemek.biernat98@gmail.com>
2023-01-02 18:08:55 +0100

    global style added

---
+++ src/GlobalStyle.js
@@ -0,0 +1,31 @@
+import {createGlobalStyle } from "styled-components";
+import background from "./Image/background.png";
+
+export const GlobalStyle = createGlobalStyle`
+html {
+  box-sizing: border-box;
+}
+
+*,
+::after,
+::before {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-image: url("${background}");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+`;
