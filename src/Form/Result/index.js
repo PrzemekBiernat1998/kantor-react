f04a21bfdbf01c737c5791d4ca159105f75fcad2
PrzemekBiernat1998 <przemek.biernat98@gmail.com>
2023-02-16 19:22:59 +0100

    idk

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -4,9 +4,9 @@ export const Result = ({ result }) => (
    <StyledResult>
         {result !== undefined && (
             <>
-                {result.startAmount.toFixed(2)} &nbsp;PLN&nbsp;=&nbsp;
+                {result?.startAmount?.toFixed(2)} &nbsp;PLN&nbsp;=&nbsp;
                 <strong>
-                    {result.finalAmount.toFixed(2)}&nbsp;{result.currencyOptions}
+                    {result?.finalAmount?.toFixed(2)}&nbsp;{result.currency}
                 </strong>
             </>
         )}
